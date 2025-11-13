34434f0f59802fc8cf6a1873f1b33373fe403b47
Ethan468614 <ethancherian555@gmail.com>
2025-11-13 10:24:54 +0530

    My first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+print("Lets add two numbers")
+a =int(input("Enter the value of a"))
+b =int (input("Enter the value of b"))
+sum= 0
+sub= 0
+mult= 0
+div= 0
+sum= a+b
+sub= a-b
+mult= a*b
+div= a/b
+print("The value of sum is", sum)
+print("The value of sub is", sub)
+print("The value of mult is", mult)
+print("The value of div is", div)
---
+++ print.py
@@ -0,0 +1,6 @@
+print("Hello my name is Ethan")
+print(4)
+print("Hello my name is Tom \n")
+print("Whats up ",5)
+print("You are welcome ", end="*")
+
