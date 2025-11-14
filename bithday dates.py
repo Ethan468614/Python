ea2b6e6fcb0613d5ce78fba5775fc86a2515aa86
Ethan468614 <ethancherian555@gmail.com>
2025-11-14 09:44:17 +0530

    birthday project

---
+++ bithday dates.py
@@ -0,0 +1,22 @@
+letter= input("Enter yor leter ")
+a= "Ethans bithday is on 26th august 2013"
+b= "Enoshs bithay date is on 31 december 2010"
+c= "Shintos birthday is on 11th of november"
+d= "Prijthas birthdays is on june 16th"
+e= "Cherians bithday is on march 5th"
+if letter == "a":
+    print(a)
+elif letter == "b":
+    print(b)
+elif letter == "c":
+    print(c)
+elif letter == "d":
+    print(d)
+else :
+    print(e)        
+
+
+
+
+
+
