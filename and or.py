fb60179d32f328e8b3d62019caf776903d202bbc
Ethan468614 <ethancherian555@gmail.com>
2025-11-26 10:28:46 +0530

    and and or operaters

---
+++ and or.py
@@ -0,0 +1,13 @@
+a = 10 
+b= 12
+c = 0
+
+if a and b and c:
+    print("All variables are true")
+else:
+    print("At least one variable is false")   
+
+if b>0 or c>0:
+    print("At least one variable is greater than zero")
+else:
+    print("No variable is greater than zero")         
