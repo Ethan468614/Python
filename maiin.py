ff19b48f5669320c81784a84fcfbf12f38b998bf
Ethan468614 <ethancherian555@gmail.com>
2025-11-28 09:55:51 +0530

    ecvation

---
+++ maiin.py
@@ -0,0 +1,21 @@
+v = 4
+
+w = 5
+
+x = 8
+
+y = 2
+
+z = 0
+z = (v + w) * x / y;
+
+print('Value of (v+w)) * x/y is:', z)
+
+name = "Ethan"
+
+age = 0
+
+if name == "Ethan" or name == "Alex":
+    print("hello")
+else:
+    print("bye")
