7d008aaa17f3c424076ab9932f432f4772af9eb3
Ethan468614 <ethancherian555@gmail.com>
2025-11-29 13:41:16 +0530

    alfabet checker

---
+++ alfabat.py
@@ -0,0 +1,40 @@
+a= "a"
+b= "b"
+c= "c"
+d= "d"
+e= "e"
+f= "f"
+g= "g"
+h= "h"
+i= "i"
+j= "j"
+k= "k"
+l= "l"
+m= "m"
+n= "n"
+o= "o"
+p= "p"
+q= "q"
+r= "r"
+s= "s"
+t= "t"
+u= "u"
+v= "v"
+w= "w"
+x= "x"
+y= "y"
+z= "z"
+
+
+
+
+
+user_input = input("Enter a letter: ")
+
+alfabet = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
+
+if user_input in alfabet:
+    print('good')
+
+else:
+    print("Invalid input")    
