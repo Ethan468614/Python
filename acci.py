a94b21352a8fe9514921848bab07779428d4e92c
Ethan468614 <ethancherian555@gmail.com>
2025-11-29 13:28:13 +0530

    ascci value

---
+++ acci.py
@@ -0,0 +1,87 @@
+a = 65
+b = 66
+c = 67
+d = 68
+e = 69
+f = 70
+g = 71
+h = 72
+i = 73
+j = 74
+k = 75
+l = 76
+m = 77
+n = 78
+o = 79
+p = 80
+q = 81
+r = 82
+s = 83
+t = 84
+u = 85
+v = 86
+w = 87
+x = 88
+y = 89
+z = 90
+
+user_input = input("Enter a letter: ")
+
+
+if user_input == "a" or user_input == "A":
+    print(a)
+elif user_input == "b" or user_input == "B":
+    print(b) 
+elif user_input == "c" or user_input == "C":
+    print(c)
+elif user_input == "d" or user_input == "D":
+    print(d)
+elif user_input == "e" or user_input == "E":
+    print(e)
+elif user_input == "f" or user_input == "F":
+    print(f)
+elif user_input == "g" or user_input == "G":
+    print(g)
+elif user_input == "h" or user_input == "H":
+    print(h)
+elif user_input == "i" or user_input == "I":
+    print(i)
+elif user_input == "j" or user_input == "J":
+    print(j)
+elif user_input == "k" or user_input == "K":
+    print(k)
+elif user_input == "l" or user_input == "L":
+    print(l)
+elif user_input == "m" or user_input == "M":
+    print(m)
+elif user_input == "n" or user_input == "N":
+    print(n)
+elif user_input == "o" or user_input == "O":
+    print(o)
+elif user_input == "p" or user_input == "P":
+    print(p)
+elif user_input == "q" or user_input == "Q":
+    print(q)
+elif user_input == "r" or user_input == "R":
+    print(r)
+elif user_input == "s" or user_input == "S":
+    print(s)
+elif user_input == "t" or user_input == "T":
+    print(t)
+elif user_input == "u" or user_input == "U":
+    print(u)
+elif user_input == "v" or user_input == "V":
+    print(v)
+elif user_input == "w" or user_input == "W":
+    print(w)
+elif user_input == "x" or user_input == "X":
+    print(x)
+elif user_input == "y" or user_input == "Y":
+    print(y)
+elif user_input == "z" or user_input == "Z":
+    print(z)
+else:   
+    print("Invalid input")
+
+
+
