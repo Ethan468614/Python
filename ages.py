022182f3d3209bb6cd29514432827b88ef205425
Ethan468614 <ethancherian555@gmail.com>
2025-12-08 11:53:29 +0530

    eligible age

---
+++ ages.py
@@ -0,0 +1,18 @@
+user_input = int(input("Enter your age: "))
+
+ages = range(10, 21)
+
+if user_input in ages:
+    print("You are eligible ")
+
+else:
+    print('You are not alllowed into this class!')
+   
+
+
+
+
+
+
+
+    
