e9b03f3ed8c418aa0bce2d1566a1b73d7adde1f9
Ethan468614 <ethancherian555@gmail.com>
2025-12-22 19:17:32 +0530

    multiplication project with  fuction

---
+++ multiplier.py
@@ -0,0 +1,16 @@
+def mult(a, b):
+    """Returns the product of a and b."""
+    return a * b
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+
+result = mult(num1, num2)
+print("The product is: ", result)
+
+
+
+
+
+
