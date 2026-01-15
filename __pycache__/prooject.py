9e94f87c1abc4af8fbde708f1ca94bc17fc0ad5d
Ethan468614 <ethancherian555@gmail.com>
2026-01-15 12:37:31 +0530

    tuple2

---
+++ __pycache__/prooject.py
@@ -0,0 +1,14 @@
+#write a python program to calculate the product multiplying all the numbers of the given tuple.
+
+def mult(tup):
+    product = 1
+    for num in tup:
+        product *= num
+    return product
+
+tuple1 = (24, 12, -5, 8, 9)
+
+tuple2 = (1, 5, 38, 23, 7)
+
+print("The product of tuple1 is:", mult(tuple1))
+print("The product of tuple2 is:", mult(tuple2))
